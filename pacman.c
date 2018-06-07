b5bb154ca2493a8b672db094bb134c2e90c79fab
Tulio Martins <tuliomartinstm1523@gmail.com>
2018-06-07 17:53:08 -0300

    algoritmo (1/3) - main sem BFS completo

--- pacman.c
+++ pacman.c
@@ -1,9 +1,16 @@
 #include "GRAPH.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 typedef struct _ResultSet {
   int first_step; // O no que eh o primeiro caminho da fonte
   int pellet_node;// O no que eh o power pellet
 } ResultSet;
+
+typedef struct _Queue {
+  int v;
+  struct _Queue *next;
+} queue, *Queue;
 
 int main() {
     /*Definindo variaveis*/
@@ -11,6 +18,7 @@ int main() {
     int n_nodes;
     int m_edges;
     Graph g;
+    Queue q = NULL;
 
     int source;
     int n_pellets;
@@ -40,8 +48,8 @@ int main() {
     for(int i = 0; i < n_ghosts; i++)
       scanf("%d", &ghosts[i]);
 
-    /*BFS*/
-    rs = BFSearch(g, source, pellets, ghosts);
+    /*BFSearch*/
+    rs = BFSearch(g, source, pellets, ghosts, q);
 
     printf("First Pellet: %d\n", rs.pellet_node);
     printf("First Step: %d\n", rs.first_step);
@@ -49,8 +57,28 @@ int main() {
     return 0;
 }
 
-ResultSet BFSearch(Graph g, int source, int pellets[], int ghosts[]) {
+ResultSet BFSearch(Graph g, int source, int pellets[], int ghosts[], Queue q) {
   ResultSet rs;
+  int stop_queuing = FALSE;
+  int possible_pellets = malloc(n*sizeof(pellets)); /*(!!!!!!!)*/
+  //int pi = source;
+
+
+  for(pAdjListNode n = g->AdjacencyList[source].head; n != NULL; n = n->next) {
+    if(notGhost(n->v)) {
+      if(isPellet(n->v)) {
+        stop_queueing = TRUE;
+      } else {
+        enqueue(n->v);
+      }
+    }
+  }
+
+  if(stop_queing) {
+
+  }
+
+
 
   return rs;
 }
